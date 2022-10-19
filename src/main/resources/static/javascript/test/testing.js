d96d2bc53bc4e02fc9d7d5a08fb65004044ec985
rio319-an <78468001+rio319-an@users.noreply.github.com>
2022-10-19 16:29:10 +0900

    testing input, radar chart

--- src/main/resources/static/javascript/test/testing.js
+++ src/main/resources/static/javascript/test/testing.js
@@ -1,14 +1,13 @@
-/*
-const detailBoxs = document.querySelectorAll('.detail__box');
-const detailInside = document.querySelector('.detail__inside');
+// const detailBoxs = document.querySelectorAll('.detail__box');
+// const detailInside = document.querySelector('.detail__inside');
 
-// 수정 필요
-detailBoxs.forEach(detailBox => {
-    detailBox.addEventListener("click", ()=> {
-        detailBox.classList.toggle('active');
-        detailInside.classList.toggle('active');
-    })
-})*/
+// // 수정 필요
+// detailBoxs.forEach(detailBox => {
+//     detailBox.addEventListener("click", () => {
+//         detailBox.classList.toggle('active');
+//         detailInside.classList.toggle('active');
+//     })
+// })
 
 const inputId = ["worst", "high", "soso", "low", "none"];
 
@@ -17,6 +16,7 @@ const groups = document.querySelectorAll(".form-group");
 for (var i = 0; i < groups.length; i++) {
     const group = groups[i];
     const checks = group.querySelectorAll(".check");
+    const result = group.querySelector(".check__result input");
     for (var j = 0; j < 5; j++) {
         const check = checks[j];
         const input = check.querySelector("input");
@@ -25,5 +25,9 @@ for (var i = 0; i < groups.length; i++) {
         input.name = "check" + i;
         input.id = inputId[j] + i;
         label.htmlFor = inputId[j] + i;
+
+        check.addEventListener('click', () => {
+            result.value = input.value;
+        })
     }
 }
