35cf3e62ccf202245c8d545cf03de57e868e9667
rio319-an <78468001+rio319-an@users.noreply.github.com>
2022-10-19 16:57:47 +0900

    box color

--- src/main/resources/static/javascript/test/my_result.js
+++ src/main/resources/static/javascript/test/my_result.js
@@ -1,6 +1,3 @@
-const articles = document.querySelectorAll(".result__list article");
-
-
 async function fetchAsText(url){
     return await (await fetch(url)).text();
 }
@@ -11,7 +8,17 @@ async function importNav(targetDiv) {
 
 importNav('nav');
 
+// my__reult.js
+const articles = document.querySelectorAll(".result__list article");
+
 articles.forEach(article => {
+
+    const box = article.querySelector(".result__box");
+    const avg_box = box.querySelector(".avg");
+
+    var avg = parseFloat(avg_box.innerText);
+    colorByAvg(avg,box);
+
     article.addEventListener('click', ()=> {
         const chart = article.querySelector('.result__chart');
         const icon = article.querySelector('.result__box i');
@@ -19,3 +26,13 @@ articles.forEach(article => {
         icon.classList.toggle('active');
     })
 })
+
+function colorByAvg(avg,box) {
+    if(avg >= 4){
+        box.style.background = '#FC5230'
+    } else if(avg >= 1) {
+        box.style.background = '#EFB93A'
+    } else {
+        box.style.background = '#1DBA84'
+    }
+}
