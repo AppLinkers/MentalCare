f5d27aeb3a375abc898502937ff393c97b9a264d
rio319-an <78468001+rio319-an@users.noreply.github.com>
2022-11-08 19:46:11 +0900

    dir-member

--- src/main/resources/static/javascript/manage/director_member.js
+++ src/main/resources/static/javascript/manage/director_member.js
@@ -1,5 +1,6 @@
 const articles = document.querySelectorAll(".member-list");
 
+/*
 for (var i = 0; i < articles.length; i++) {
     const article = articles[i];
     const input = article.querySelector("input");
@@ -7,4 +8,16 @@ for (var i = 0; i < articles.length; i++) {
 
     input.id = "switch" + i;
     label.htmlFor = "switch" + i;
-}
+}*/
+articles.forEach(article => {
+    const permission = article.querySelector("button");
+    permission.addEventListener("click", () => {
+        permission.classList.toggle("active");
+        if (!permission.classList.contains("active")) {
+            permission.innerText = "권한 없음";
+        }
+        else {
+            permission.innerText = "허가됨";
+        }
+    })
+})
