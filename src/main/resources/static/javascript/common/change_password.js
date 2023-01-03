d061c7792ec9672b0b63e640dcc37b3a58bdc9bb
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-01-03 20:07:36 +0900

    change_password.js fix

--- src/main/resources/static/javascript/common/change_password.js
+++ src/main/resources/static/javascript/common/change_password.js
@@ -2,7 +2,9 @@ const checkPassword = function () {
     if (document.getElementById('pw').value ===
         document.getElementById('pwCheck').value){
         document.querySelector('.pwCheck-message').classList.remove('active');
+        document.getElementById('submit').disabled = false;
     }   else {
         document.querySelector('.pwCheck-message').classList.add('active');
+        document.getElementById('submit').disabled = true;
     }
 }
