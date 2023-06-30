34bf4d9a4eaace64d996ebee94f51f26a7497cca
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-06-30 15:55:16 +0900

    Fix : 리뉴얼 파일 추가

--- src/main/resources/static/javascript/common/signup.js
+++ src/main/resources/static/javascript/common/signup.js
@@ -1,52 +1,16 @@
-const optionMenu = document.querySelector(".select-menu"),
-    selectBtn = optionMenu.querySelector(".select-btn"),
-    options = optionMenu.querySelectorAll(".option"),
-    sBtn_text = optionMenu.querySelector(".sBtn-text");
+const form = document.querySelector(".form__container");
+const team_code = document.querySelector(".team_code");
+const position = document.querySelector(".position");
 
-const role = document.getElementById("role");
-
-selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
-
-options.forEach(option => {
-    option.addEventListener("click", () => {
-        let selectedOption = option.querySelector(".option-text").innerText;
-        role.value= selectedOption;
-        optionMenu.classList.remove("active");
-    })
-})
-
-const pro = document.getElementById("pro");
-const dir = document.getElementById("dir");
-const form_pro = document.getElementById("form-pro");
-const form_dir = document.getElementById("form-dir");
-
-pro.addEventListener("click", () => {
-    form_dir.classList.remove("active");
-    form_pro.classList.add("active");
-})
-
-dir.addEventListener("click", () => {
-    form_pro.classList.remove("active");
-    form_dir.classList.add("active");
-})
-
-const password = document.getElementById('password');
-const re_password = document.getElementById('re-password');
-const dir_password = document.getElementById('password-dir');
-const dir_re_password = document.getElementById('re-password-dir');
-
-function passwordCheck(){
-    if(password.value == re_password.value){
-        re_password.setCustomValidity('');
-    }else{
-        re_password.setCustomValidity("비밀번호를 확인해주세요.");
+function selectChange() {
+    team_code.style.display = "block";
+    position.style.display = "block";
+    var select = document.getElementById("role").options[document.getElementById("role").selectedIndex].value;
+    if (select === "player_personal") {
+        team_code.style.display = "none";
     }
-}
-
-function passwordDirCheck(){
-    if(dir_password.value == dir_re_password.value){
-        dir_re_password.setCustomValidity('');
-    }else{
-        dir_re_password.setCustomValidity("비밀번호를 확인해주세요.");
+    if (select === "director" || select === "consultant") {
+        position.style.display = "none";
     }
+    form.style.display = "block";
 }
